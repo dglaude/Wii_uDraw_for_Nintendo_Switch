6a6b9b400a09f32e489fa2770fcc6fc168eb4d88
David Glaude <dglaude@users.noreply.github.com>
2022-04-22 22:54:55 +0200

    Initial working version
    As found on my Feather RP2040.

---
+++ code.py
@@ -0,0 +1,58 @@
+# SPDX-FileCopyrightText: 2022 David Glaude
+# SPDX-License-Identifier: MIT
+
+import board
+import usb_hid
+from adafruit_hid.mouse import Mouse
+from wiichuck.udraw import UDraw
+
+# To create I2C bus on specific pins
+
+# i2c = busio.I2C(board.SCL1, board.SDA1)  # QT Py RP2040 STEMMA connector
+# udraw = UDraw(i2c)
+
+udraw = UDraw(board.STEMMA_I2C())
+
+m = Mouse(usb_hid.devices)
+
+zDown = False
+pDown = False
+
+oldx = 4095
+oldy = 4095
+
+while True:
+    # We don't use the pressure, maybe converting to watcom protocol would permit that
+    position, buttons, _ = udraw.values
+
+    P = buttons.tip or buttons.C  # Both the C button and tip work as LEFT mouse click
+
+    # Handeling the button to create mouse click
+    if P and not pDown:
+        m.press(Mouse.LEFT_BUTTON)
+        pDown = True
+    elif not P and pDown:
+        m.release(Mouse.LEFT_BUTTON)
+        pDown = False
+    if buttons.Z and not zDown:
+        m.press(Mouse.RIGHT_BUTTON)
+        zDown = True
+    elif not buttons.Z and zDown:
+        m.release(Mouse.RIGHT_BUTTON)
+        zDown = False
+    # Values (4095,4095) mean the pen is not near the tablet
+    if position.x == 4095 or position.y == 4095:
+        oldx = 4095
+        oldy = 4095
+        continue  # We remember that the pen was raised UP
+    # If we reach here, the pen was UP and is now DOWN
+    if oldx == 4095 or oldy == 4095:
+        oldx = position.x
+        oldy = position.y
+        continue
+    if (position.x != oldx) or (
+        position.y != oldy
+    ):  # PEN has moved we move the HID mouse
+        m.move((position.x - oldx), (oldy - position.y), 0)
+        oldx = position.x
+        oldy = position.y
